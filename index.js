be428310255811a59ef905ba61c2de6b57fc3193
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2021-08-27 19:40:49 +0600

    array object prop

--- index.js
+++ index.js
@@ -7,16 +7,18 @@ var students = {
     name: "",
     age : "",
     gender: "",
-    country : ""
+    country : "",
+    language : "[]",
 }
 
 /* creating objects with constructor */
 
-function Students(name,age,gender,country) {
+function Students(name,age,gender,country,language) {
     this.name = name;
     this.age = age;
     this.gender = gender;
     this.country = country;
+    this.language = language;
 }
 
 students = new Students();
@@ -24,6 +26,7 @@ students.name = "John Smith";
 students.age = 23;
 students.gender = "male";
 students.country = "United States";
+students.language = "[English, Spanish, Latin]";
 
 console.log(students);
-document.write("name: "+students.name);
+document.write("Language "+students.language);
