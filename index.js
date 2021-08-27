0ae4af8530abf91f198af05c10376859e3d72bf9
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2021-08-27 19:36:25 +0600

    object

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 document.write("Hello JavaScript"); /* write html in js */
 console.log("Hello"); /* console */
+
+/* objects */
+
+var students = {
+    name: "",
+    age : "",
+    gender: "",
+    country : ""
+}
+
+/* creating objects with constructor */
+
+function Students(name,age,gender,country) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.country = country;
+}
+
+students = new Students();
+students.name = "John Smith";
+students.age = 23;
+students.gender = "male";
+students.country = "United States";
+
+console.log(students);
+document.write("name: "+students.name);
